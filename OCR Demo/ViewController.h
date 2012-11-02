683a736bc2e4fb83ca9d84548c79fa8822b8d1bd
Brian Reber <reber.brian@gmail.com>
2012-11-02 09:27:16 -0500

    Comment the implementation

--- OCR Demo/ViewController.h
+++ OCR Demo/ViewController.h
@@ -9,11 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate> {
+    
+    // The UITextView that will contain the results
     IBOutlet UITextView *result;
+    
+    // The UIImageView that will contain the image being OCR'd
     IBOutlet UIImageView *imageView;
+    
+    // An activity indicator indicating whether the OCR is processing
     IBOutlet UIActivityIndicatorView *activity;
 }
 
+// The method to be called that will pop up the image picker
 - (IBAction)takePicture;
+
+// Update the UI when processing is completed
+- (void)updateUI:(NSString *)text;
 
 @end
--- OCR Demo/ViewController.m
+++ OCR Demo/ViewController.m
@@ -21,55 +21,78 @@
 {
     [super viewDidLoad];
     
+    // Initialize the Tesseract instance with the trained data found in our bundle
     self.tesseract = [[Tesseract alloc] initWithDataPath:@"tessdata" language:@"eng"];
+    
+    // Set the character whitelist to just be numbers, lowercase and uppercase letters
+    // and a few special symbols
     [self.tesseract setVariableValue:@"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-=*/"
                               forKey:@"tessedit_char_whitelist"];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)takePicture
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)takePicture {
+    // Create the image picker view controller
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    [imagePicker setAllowsEditing:YES];
     
+    // Check to see if the device has a camera
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        // If it has a camera, use the camera w/ editing feature
         [imagePicker setSourceType:UIImagePickerControllerSourceTypeCamera];
-        [imagePicker setAllowsEditing:YES];
     } else {
+        // If it doesn't have a camera, allow the user to choose from
+        // their photo library
         [imagePicker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
     }
     
+    // Set the delegate to self so we get notified when the image has been chosen
     [imagePicker setDelegate:self];
     
+    // Present the UIImagePickerController
     [self presentViewController:imagePicker animated:YES completion:nil];
 }
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
+    // Get the image that was chosen (the edited image
     UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    // Display the image on the screen
     [imageView setImage:image];
+    
+    // Start animating the activity indicator
     [activity startAnimating];
+    
+    // Hide the UITextView while we are processing
     [result setHidden:YES];
+    
+    // Start processing the image in the background
     [self performSelectorInBackground:@selector(performRecognition:) withObject:image];
     
+    // Hide the UIImagePickerController
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-
-- (NSString *)performRecognition:(UIImage *)image {
+- (NSString *)performRecognition:(UIImage *)image
+{
+    // Start processing the image
     [self.tesseract setImage:image];
     [self.tesseract recognize];
     
-    [self performSelectorOnMainThread:@selector(updateUI:) withObject:[self.tesseract recognizedText] waitUntilDone:YES];
+    // When we are done processing, perform the updateUI method
+    // on the main thread
+    [self performSelectorOnMainThread:@selector(updateUI:)
+                           withObject:[self.tesseract recognizedText]
+                        waitUntilDone:YES];
     
     return [self.tesseract recognizedText];
 }
 
-- (void)updateUI:(NSString *)text {
+- (void)updateUI:(NSString *)text
+{
+    // Update the text, stop animating the activity indicator,
+    // and show the text view again
     result.text = text;
     [activity stopAnimating];
     [result setHidden:NO];
